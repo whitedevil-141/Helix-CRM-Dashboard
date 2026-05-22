d149e27cb2bc9f1de51eda1fa240d9c061b1917b
WHITE DEVIL <white.devil.dev.141@gmail.com>
2026-05-23 02:27:56 +0600

    Fix script source path in index.html and set base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/Helix-CRM-Dashboard/',
   plugins: [react()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
   test: {
